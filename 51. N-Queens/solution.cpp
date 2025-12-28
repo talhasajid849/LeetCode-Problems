0395785e92a8ef1eaf8ad0f4791331f80094902f
Talha Sajid <rts849@outlook.com>
2025-12-28 11:13:28 +0500

    Implement N-Queens solution using backtracking algorithm

---
+++ 51. N-Queens/solution.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    vector<vector<string>> result;
+    vector<string> board;
+    int n;
+
+    bool isSafe(int row, int col) {
+        for (int i = 0; i < row; i++) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
+
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q')
+                return false;
+        }
+
+        for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
+            if (board[i][j] == 'Q')
+                return false;
+        }
+        return true;
+    }
+
+    void backtrack(int row) {
+        if (row == n) {
+            result.push_back(board);
+            return;
+        }
+
+        for (int col = 0; col < n; col++) {
+            if (isSafe(row, col)) {
+                board[row][col] = 'Q';
+                backtrack(row + 1);
+                board[row][col] = '.';
+            }
+        }
+    }
+
+    vector<vector<string>> solveNQueens(int n) {
+        this->n = n;
+        board.assign(n, string(n, '.'));
+        backtrack(0);
+        return result;
+    }
+};
